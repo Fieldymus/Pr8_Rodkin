229263137ff260046f5ea0cd5ca39818ed3baecd
Fieldymus <Fieldymus@gmail.com>
2022-11-17 00:12:18 +0300

    Extend dev Password Generator

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ pas = ''
 for x in range(11):
     pas = pas + \
         random.choice(
-            list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZабвгдеёжзийклмнопрстуфхцчшщъьэюя'))
+            list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZабвгдеёжзийклмнопрстуфхцчшщъьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЬЭЮЯ'))
 print('Your password is: ', pas)
 
